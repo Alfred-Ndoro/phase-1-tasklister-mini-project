2071725db0da59a8ca51c9e5d12a83b637ea99a3
Alfred-Ndoro <alfred.ndoro@outlook.com>
2025-10-24 10:06:23 +0300

    assignmentsubmission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+
+    const taskDescription = document.getElementById("new-task-description").value;
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskDescription;
+
+    const taskList = document.getElementById("tasks");
+    if (taskList) {
+      taskList.appendChild(taskItem);
+    }
+  })
 });
